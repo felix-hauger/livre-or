e5e5a1a72dd82badad66f83ec04e2c162290024f
felix-hauger <felix.hauger@laplateforme.io>
2022-12-05 18:45:50 +0100

    added log out function in header.php navbar from deconnexion.php

--- elements/header.php
+++ elements/header.php
@@ -4,8 +4,7 @@
             <li><a href="index.php">Accueil</a></li>
             <li><a href="inscription.php">Inscription</a></li>
             <li><a href="connexion.php">Connexion</a></li>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="index.php">Accueil</a></li>
+            <li><a href="deconnexion.php">Accueil</a></li>
         </ul>
     </nav>
 </header>
