5410d270d2743bc91785c0ac9d2051806f0a3b96
felix-hauger <felix.hauger@laplateforme.io>
2022-12-05 18:46:46 +0100

    header.php - fixed log out navbar text

--- elements/header.php
+++ elements/header.php
@@ -4,7 +4,7 @@
             <li><a href="index.php">Accueil</a></li>
             <li><a href="inscription.php">Inscription</a></li>
             <li><a href="connexion.php">Connexion</a></li>
-            <li><a href="deconnexion.php">Accueil</a></li>
+            <li><a href="deconnexion.php">Déconnexion</a></li>
         </ul>
     </nav>
 </header>
