32080dcb911826924d2efa186ea00b245ff372ea
felix-hauger <felix.hauger@laplateforme.io>
2022-12-06 09:54:21 +0100

    header.php - added comment.php link

--- elements/header.php
+++ elements/header.php
@@ -5,6 +5,7 @@
             <li><a href="guestbook.php">Livre d'Or</a></li>
             <li><a href="inscription.php">Inscription</a></li>
             <li><a href="connexion.php">Connexion</a></li>
+            <li><a href="comment.php">Ajouter un Commentaire</a></li>
             <li><a href="deconnexion.php">Déconnexion</a></li>
         </ul>
     </nav>
