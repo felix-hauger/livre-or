fba0c3fcd88d8829acec60a3849a3ba24acf0173
felix-hauger <felix.hauger@laplateforme.io>
2022-12-06 09:48:59 +0100

    added guest guestbook.php page + link in header.php

--- elements/header.php
+++ elements/header.php
@@ -2,6 +2,7 @@
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
+            <li><a href="guestbook.php">Livre d'Or</a></li>
             <li><a href="inscription.php">Inscription</a></li>
             <li><a href="connexion.php">Connexion</a></li>
             <li><a href="deconnexion.php">Déconnexion</a></li>
