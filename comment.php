ecdd998f9af10136a445c85b051a8723bd03f654
felix-hauger <felix.hauger@laplateforme.io>
2022-12-08 15:44:15 +0100

    comment.php - add comment in database linked to logged user

--- comment.php
+++ comment.php
@@ -1,4 +1,30 @@
 <?php
+session_start();
+
+var_dump($_SESSION);
+if (isset($_SESSION['is_logged']) && isset($_SESSION['logged_user_id'])) {
+
+    
+    if (isset($_POST['submit'])) {
+        require_once 'functions/db_connect.php';
+        
+        if (!empty($_POST['comment'])) {
+            $comment = htmlspecialchars(trim($_POST['comment']));
+            $logged_user_id = $_SESSION['logged_user_id'];
+            
+            $sql = 'INSERT into comments (`comment`, `user_id`, `date`) VALUES (:comment, :user_id, :date)';
+            
+            $insert = $pdo->prepare($sql);
+            $insert->execute([
+                'comment' => $comment,
+                'user_id' => $logged_user_id,
+                'date'    => date('Y-m-d h:m:s')
+            ]);
+            
+        } 
+    }
+
+}
 
 ?>
 <!DOCTYPE html>
