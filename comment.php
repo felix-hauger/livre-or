a927fdd069908441e8650c7811a2ed01b47ce22e
felix-hauger <felix.hauger@laplateforme.io>
2022-12-08 10:00:41 +0100

    comment.php - added html form & updated title

--- comment.php
+++ comment.php
@@ -7,9 +7,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter un commentaire | Livre d'Or</title>
+    <title>Ajouter un Commentaire | Livre d'Or</title>
 </head>
 <body>
     <?php require_once 'elements/header.php'; ?>
+    <h1>Ajouter un Commentaire</h1>
+    <form action="" method="post">
+        <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+        <input type="submit" name="submit" value="Envoyer">
+    </form>
 </body>
 </html>
