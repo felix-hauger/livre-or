0e3651bc210f5bda46f9c3235cacc5e45cae525a
felix-hauger <felix.hauger@laplateforme.io>
2022-12-08 16:49:36 +0100

    header.php - added conditional display for nav links

--- elements/header.php
+++ elements/header.php
@@ -3,11 +3,14 @@
         <ul>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="guestbook.php">Livre d'Or</a></li>
-            <li><a href="inscription.php">Inscription</a></li>
-            <li><a href="connexion.php">Connexion</a></li>
+            <?php if (isset($_SESSION['is_logged'])): ?>
             <li><a href="comment.php">Ajouter un Commentaire</a></li>
             <li><a href="profil.php">Profil</a></li>
             <li><a href="deconnexion.php">Déconnexion</a></li>
+            <?php else: ?>
+            <li><a href="inscription.php">Inscription</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
+            <?php endif ?>
         </ul>
     </nav>
 </header>
