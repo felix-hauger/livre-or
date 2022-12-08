e7fc2fbc5113539be46cd7c84457f365ac20d9fe
felix-hauger <felix.hauger@laplateforme.io>
2022-12-08 15:51:00 +0100

    guestbook.php - sort comments by date & removed var_dump

--- guestbook.php
+++ guestbook.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'functions/db_connect.php';
 
-$sql = 'SELECT comment, date, users.login FROM comments INNER JOIN users ON comments.user_id = users.id';
+$sql = 'SELECT comment, date, users.login FROM comments INNER JOIN users ON comments.user_id = users.id ORDER BY date DESC';
 
 // statement
 $stmt = $pdo->prepare($sql);
@@ -26,11 +26,9 @@ $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <h1>Livre d'Or</h1>
     <h2>Vos commentaires</h2>
     <main>
-        <?php 
-        var_dump($comments);
-        // var_dump($comment);
-        foreach ($comments as $comment): ?>
-            <article><!-- Format string to timestamp Unix w/ strtotime, then format it with date function -->
+        <?php foreach ($comments as $comment): ?>
+
+            <article><!-- Format string to timestamp Unix w/ strtotime, then format it w/ date function -->
                 <h3>Posté le <?= date('d/m/Y', strtotime($comment['date'])) ?> par <?= $comment['login'] ?></h3>
                 <p><?= $comment['comment'] ?></p>
             </article>
