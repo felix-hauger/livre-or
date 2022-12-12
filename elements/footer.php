e943428ab74b947a8ea4b911fb0144f5fd9c73e2
felix-hauger <felix.hauger@laplateforme.io>
2022-12-12 18:58:58 +0100

    updated header & footer elements

--- elements/footer.php
+++ elements/footer.php
@@ -1,1 +1,6 @@
-<footer>pieddepage</footer>
+    <footer>
+        <a href="https://felix-hauger.github.io/portfolio/">Portfolio</a>
+        <a href="https://www.linkedin.com/in/f%C3%A9lix-hauger/">LinkedIn</a>
+    </footer>
+</body>
+</html>
--- elements/header.php
+++ elements/header.php
@@ -1,15 +1,19 @@
 <header>
+    <h1>Livre d'Or</h1>
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="livre-or.php">Livre d'Or</a></li>
+
             <?php if (isset($_SESSION['is_logged'])): ?>
-            <li><a href="commentaire.php">Ajouter un Commentaire</a></li>
-            <li><a href="profil.php">Profil</a></li>
-            <li><a href="deconnexion.php">Déconnexion</a></li>
+                <li><a href="commentaire.php">Ajouter un Commentaire</a></li>
+                <li><a href="profil.php">Profil</a></li>
+                <li><a href="deconnexion.php">Déconnexion</a></li>
+
             <?php else: ?>
-            <li><a href="inscription.php">Inscription</a></li>
-            <li><a href="connexion.php">Connexion</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
+                
             <?php endif ?>
         </ul>
     </nav>
