4e0668a1bc4f9495ec8cc5e0d1bfc666c23bb6ae
felix-hauger <felix.hauger@laplateforme.io>
2022-12-14 14:47:27 +0100

    removed useless closing tags in footer elements

--- elements/footer-static.php
+++ elements/footer-static.php
@@ -1,6 +1,4 @@
-    <footer style="position: static">
-        <a href="https://github.com/felix-hauger"><i class="fa-brands fa-square-github"></i></a>
-        <a href="https://www.linkedin.com/in/f%C3%A9lix-hauger/"><i class="fa-brands fa-linkedin"></i></a>
-    </footer>
-</body>
-</html>
+<footer style="position: static">
+    <a href="https://github.com/felix-hauger"><i class="fa-brands fa-square-github"></i></a>
+    <a href="https://www.linkedin.com/in/f%C3%A9lix-hauger/"><i class="fa-brands fa-linkedin"></i></a>
+</footer>
