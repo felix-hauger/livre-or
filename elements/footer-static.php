ae4eccbea31320b2430dfd022a4b3dfd0c88a743
felix-hauger <felix.hauger@laplateforme.io>
2022-12-15 13:16:15 +0100

    updated footer icons in elements/footer-static.php

--- elements/footer-static.php
+++ elements/footer-static.php
@@ -1,4 +1,4 @@
 <footer style="position: static">
-    <a href="https://github.com/felix-hauger"><i class="fa-brands fa-square-github"></i></a>
-    <a href="https://www.linkedin.com/in/f%C3%A9lix-hauger/"><i class="fa-brands fa-linkedin"></i></a>
+    <a href="https://github.com/felix-hauger"><i class="fa-brands fa-github"></i></a>
+    <a href="https://www.linkedin.com/in/f%C3%A9lix-hauger/"><i class="fa-brands fa-linkedin-in"></i></a>
 </footer>
