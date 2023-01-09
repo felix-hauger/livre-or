cb60beffdff23db7a50ec29d4b84f9d30124c4ce
felix-hauger <felix.hauger@laplateforme.io>
2023-01-09 14:03:29 +0100

    db_connect.php - added comment to help config database

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-
+//  Separated by commas: Data Source Name, database login, database password
 $pdo = new PDO('mysql:dbname=livreor;host=localhost;charset=utf8mb4', 'root', '');
 
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
