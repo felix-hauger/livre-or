ec733c7f2129152e76f76c93a004c208f92c9611
felix-hauger <felix.hauger@laplateforme.io>
2023-01-23 11:20:58 +0100

    added html for delete account page

---
+++ supprimer-compte.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+// var_dump($_SESSION);
+if (!isset($_SESSION['is_logged'])) {
+    header('Location: index.php');
+    die();
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier les Informations de Profil | Livre d'Or</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/42d5a324f0.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php require_once('elements/header.php'); ?>
+
+    <main>
+        <div class="container form-container">
+            <form action="" method="post" id="delete-confirm">
+                <h2>Voulez-vous vraiment supprimer votre compte ? Cette action n'est pas réversible.</h2>
+                <input type="submit" id="delete" name="delete-confirm" value="Supprimer">
+            </form>
+        </div>
+    </main>
+
+    <?php require_once('elements/footer.php'); ?>
+</body>
+
+</html>
