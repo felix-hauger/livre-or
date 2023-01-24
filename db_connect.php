b2a319a40f961e99f0ae1e6f94a2c849cc13f832
felix-hauger <felix.hauger@laplateforme.io>
2023-01-24 19:23:43 +0100

    added database.ini in config folder to modify database settings

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,15 @@
 <?php
+
+$db = parse_ini_file('config/database.ini');
+
+$type = $db['type'];
+$name = $db['name'];
+$host = $db['host'];
+$user = $db['user'];
+$password = $db['password'];
+
 //  Separated by commas: Data Source Name, database login, database password
-$pdo = new PDO('mysql:dbname=livreor;host=localhost;charset=utf8mb4', 'root', '');
+$pdo = new PDO($type . ':dbname=' . $name . ';host=' . $host . ';charset=utf8mb4', $user, $password);
 
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
