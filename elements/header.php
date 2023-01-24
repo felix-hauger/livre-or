5c02337d52f9ffe3c3f15c9cdd65c45341758afa
felix-hauger <felix.hauger@laplateforme.io>
2023-01-24 16:07:03 +0100

    elements/header.php - reverse order of links

--- elements/header.php
+++ elements/header.php
@@ -11,8 +11,8 @@
                 <li><a href="deconnexion.php">Déconnexion</a></li>
 
             <?php else: ?>
-                <li><a href="inscription.php">Inscription</a></li>
                 <li><a href="connexion.php">Connexion</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
                 
             <?php endif ?>
         </ul>
