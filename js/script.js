f7a543160c961bec69e94f40fd3700b8a8677e44
felix-hauger <felix.hauger@laplateforme.io>
2023-01-24 16:25:28 +0100

    js/script.js - display only part of the file name if it is too long

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const fileInput = document.querySelector('#profile-picture');
 const showUploadedFile = function() {
     let paragraph = document.querySelector('#pfp-name'),
         fileName = fileInput.files[0].name;
-    paragraph.innerHTML = fileName;    
+    fileName = fileName.length > 15 ? fileName.substring(0, 15) + '...' : fileName; 
+    paragraph.innerHTML = fileName;
 }
 
 fileInput.addEventListener('change', showUploadedFile);
